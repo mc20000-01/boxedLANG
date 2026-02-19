be3b886841707cbb4db8ffddedacaf19402d47dd
mc20000-01 <mc20000scratch@gmail.com>
2026-02-19 12:59:35 -0800

    almost time for 1.1 to be done

---
+++ build/lib/boxrun/box_to_json.py
@@ -0,0 +1,54 @@
+lpv = [0]
+
+def pull_cmd_from(box_line, ln=0):
+    alr_loops = 0
+    cur_loop = 0
+    global lpv
+
+    data = box_line.split(" ")
+    cmd = data[0] #gets command from box standard
+    length = len(data)
+    args = data[length - 1].split("|") #splits args from box standerd
+    if cmd == "premark":
+        marks = {data[1]: ln + 1}
+    else:
+        marks = {}
+    if cmd == "loop'":
+        alr_loops = alr_loops + 1
+        cur_loop = cur_loop + 1
+        cmd = "mrkst"
+        lpv = [*lpv, *[args[0]]]
+        args = ["sys-lp-" + str(alr_loops)]
+    if cmd == "'":
+        cmd = "jumpif"
+        lp = "sys-lp-" + str(cur_loop + 1)
+        args = ["$"+lp,">=",int(lpv[cur_loop + 1]),lp, "m"]
+        cur_loop = cur_loop - 1
+        print(lpv)
+    json = {"cmd": cmd,"args": args, "ln": ln, "marks": marks} #makes the json to be run / decoeded further
+    return json
+
+def make_code_from(code):
+    global lpv
+    made_code = [{'cmd': 'null', 'args': ['start'], 'ln': 0, 'marks': {}},]
+    code = code.splitlines()
+    for l in code:
+        cur_line = l
+        cur_line_json  = pull_cmd_from(cur_line, ln=code.index(l))
+        made_code = made_code  + [cur_line_json]
+    return made_code
+
+def mk(code):
+    return make_code_from(code)
+
+def undo_mk(boxed_code):
+    code = ""
+    for l in boxed_code:
+        cmd = l['cmd']
+        args = l['args']
+        line = cmd + " "
+        for a in args:
+            line = line + str(a) + "|"
+        line = line[:-1] + "\n"
+        code = code + line
+    return code
